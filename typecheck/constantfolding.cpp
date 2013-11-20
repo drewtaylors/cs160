f82d2972038011c0910c74e906428288c1a9036b
Ramon Rovirosa <ramon.rovirosa@gmail.com>
2013-11-19 20:22:03 -0800

    lets test this now

--- typecheck/constantfolding.cpp
+++ typecheck/constantfolding.cpp
@@ -222,6 +222,7 @@ public:
     // Join the original "in" lattice_elem_map with the clone,
     // storing the result in the clone
    join_lattice_elem_maps(clone, in);
+   join_lattice_elem_maps(clone2,in);
    join_lattice_elem_maps(clone,clone2);
    
    // Make "in" point to the clone, deleting in
