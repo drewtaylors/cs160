c64d6348f86d346369cd40b30ccea0fa8b787845
Ramon Rovirosa <ramon.rovirosa@gmail.com>
2013-11-30 19:56:41 -0800

    her

--- codegen/codegen.cpp
+++ codegen/codegen.cpp
@@ -214,7 +214,7 @@ public:
   {
     p -> visit_children(this);
     fprintf( m_outputfile, "#### RETURN\n");
-//    fprintf( m_outputfile, "popl %%eax\n");
+    fprintf( m_outputfile, "popl %%eax\n");
 
     //fprintf( m_outputfile, "movl $%d, %%eax\n",p -> m_attribute.m_lattice_elem.value);
 //    fprintf( m_outputfile, "\tret\n");
