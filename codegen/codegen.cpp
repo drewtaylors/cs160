73c632d4b293ec2c2a6c406f0fd14b7505cd40dd
Ramon Rovirosa <ramon.rovirosa@gmail.com>
2013-12-02 13:14:19 -0800

    hurdurr

--- codegen/codegen.cpp
+++ codegen/codegen.cpp
@@ -204,9 +204,8 @@ public:
         int counter=0;
         //push input params right to left
         list<Expr_ptr>::iterator iter;
-        printf("%i", p->m_attribute.m_lattice_elem.value);
          for(iter = p->m_expr_list->end(); iter != p->m_expr_list->begin(); iter--){
-            fprintf(m_outputfile, "pushl $%d\n", iter->m_attribute.m_lattice_elem.value);
+            fprintf(m_outputfile, "pushl $%d\n", &*iter->m_attribute.m_lattice_elem.value);
             counter+=4;
          }
         char *name = strdup(p -> m_symname_2 -> spelling());
