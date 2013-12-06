c6aa45322bd22d2491636b31a3de03549187e823
ramon.rovirosa@gmail.com <ramonrovirosa@gmail.com>
2013-12-06 14:49:37 -0800

    bug in array call

--- codegen/sample.s
+++ codegen/sample.s
@@ -1,69 +1,33 @@
 .text
 
+####INSIDE FUNCTION
+_foo: 
+push %ebp
+mov %esp, %ebp
+sub $0,%esp
+#### visitFuncBlock
+#### Visit INT
+pushl $5
+#### RETURN
+popl %eax
+mov %ebp, %esp 
+pop %ebp
+	ret
+
 .globl Main
 Main:
 push %ebp
 mov %esp, %ebp
-sub $8,%esp
+sub $168,%esp
 #### visitFuncBlock
+####VISIT ARRAY CALL
+call _foo
+movl %eax, -132(%ebp)
+#### Visit ArrayAccess
 #### Visit INT
-pushl $5
-#### ASSIGN
-popl %eax
-mov %eax, -8(%ebp)
-#### VISIT FOR LOOP
-#### Visit INT
-pushl $0
-#### ASSIGN
-popl %eax
-mov %eax, -4(%ebp)
-jmp compare_expr0
-func_body0:
-#### PLUS
-#### Visit ID
-pushl -8(%ebp)
-#### Visit INT
-pushl $1
-popl %ebx
-popl %eax
-addl %ebx, %eax
-pushl %eax
-#### ASSIGN
-popl %eax
-mov %eax, -8(%ebp)
-#### PLUS
-#### Visit ID
-pushl -4(%ebp)
-#### Visit INT
-pushl $1
-popl %ebx
-popl %eax
-addl %ebx, %eax
-pushl %eax
-#### ASSIGN
-popl %eax
-mov %eax, -4(%ebp)
-compare_expr0:
-#### Visit ID
-pushl -4(%ebp)
-#### Visit INT
-pushl $5
-popl %ebx
-popl %eax
-cmp %ebx,%eax
-jle equal1
-pushl $0
-jmp end1
-equal1:
-pushl $1
-end1:
-pop %ebx
-mov $1, %eax
-cmp %ebx,%eax
-je func_body0
-#### END FOR LOOP
-#### Visit ID
-pushl -8(%ebp)
+pushl $2
+pop %eax
+push -132(%ebp)
 #### RETURN
 popl %eax
 mov %ebp, %esp 
